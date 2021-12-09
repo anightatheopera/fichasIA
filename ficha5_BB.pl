28fc346fcc1e455cdcbad365b2c5fd04d2cb1e6c
ritarded <ritalino7@hotmail.com>
2021-12-09 18:36:54 +0000

    finally fixed bfs

--- ficha5_BB.pl
+++ ficha5_BB.pl
@@ -1,27 +1,50 @@
-% alínea a
+inicial(jarros(0,0)).
+
+final(jarros(4,_)).
+final(jarros(_,4)).
+
+transicao(jarros(V1,V2),encher(1),jarros(8,V2))      :- V1<8.
+transicao(jarros(V1,V2),encher(2),jarros(V1,5))      :- V2<5.
+transicao(jarros(V1,V2),esvaziar(1),jarros(0,V2))    :- V1>0.
+transicao(jarros(V1,V2),esvaziar(2),jarros(V1,0))    :- V2>0.
+transicao(jarros(V1,V2),transferir(1,2),jarros(NV1,NV2)) :- 
+    A is 5-V2,
+    Dif is min(A,V1),
+    NV1 is V1- Dif,
+    NV2 is V2+Dif.
+transicao(jarros(V1,V2),transferir(2,1),jarros(NV1,NV2)) :- 
+    A is 8-V1,
+    Dif is min(A,V2),
+    NV1 is V1+Dif,
+    NV2 is V2-Dif.
 
 
-solveDF(Ei, Ef, Path) :-
-    solveDF(Ei, Ef, [], Path).
+% coisa em dfs
+dfsYuh(Solucao) :- 
+    inicial(Inicial),
+    dfsYuh(Inicial,[Inicial],Solucao).
 
-solveDF(Ei, Ei, Visited, Path) :- reverse(Visited, Path).
-solveDF(Ei, Ef, Visited, Path) :-
-    not(member(Ei, Visited)),
-    t(Ei, E, T),
-    solveDF(E, Ef, [Ei|Visited], Path).
+dfsYuh(Estado, Vis,[]) :- 
+    final(Estado),
+    !,
+    write(Vis).
+dfsYuh(Estado,Vis,[Move|Solucao]) :- 
+    transicao(Estado,Move,Estado1),
+    not(member(Estado1,Vis)), 
+    dfsYuh(Estado1,[Estado1|Vis],Solucao).
 
+%coisa em bfs
+bfsYuh(Solucao) :- bfsYuh(jarros(0,0),jarros(_,4),Solucao).
 
-t((Xi, Yi), (8, Yi), t1x).
-t((Xi, Yi), (Xi, 5), t1y).
-t((Xi, Yi), (0, Yi), t2x).
-t((Xi, Yi), (Xi, 0), t2y).
-t((Xi, Yi), (Xf,Yf), t3x) :- 
-    A is 8 - Xi,
-    Diff is min(A,Yi),
-    Xf is Xi + Diff,
-    Yf is Yi - Diff.
-t((Xi, Yi), (Xf,Yf), t3y) :- 
-    A is 5 - Yi,
-    Diff is min(A,Xi),
-    Xf is Xi - Diff,
-    Yf is Yi + Diff.
+bfsYuh(Orig, Dest, Cam):- bfs3(Dest,[[Orig]],Cam).
+
+bfs3(EstadoF,[[EstadoF|T]|_],Solucao)  :- reverse([EstadoF|T],Solucao).
+bfs3(EstadoF,[EstadoA|Outros],Solucao) :- 
+    EstadoA = [Act|_],
+    findall([X|EstadoA],
+            (EstadoF\==Act,
+            transicao(Act,Move,X),
+            not(member(X,EstadoA))),
+            Novos),
+    append(Outros,Novos,Todos),
+    bfs3(EstadoF,Todos,Solucao).
